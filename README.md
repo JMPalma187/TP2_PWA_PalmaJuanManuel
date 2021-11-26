8fa762bf580478e8d8b14dba3ede6ae21a54e348
JMPalma187 <90937569+JMPalma187@users.noreply.github.com>
2021-11-25 23:05:34 -0300

    Beta 0.85
    Más cambios, próximos al MVP

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
-# TP2 PWA: Palma Juan Manuel | DW3ATN - 2021
-
-Trabajo Prático 2 de Aplicaciones Web Progresivas.
-
-No comments so far...
-
+# TP2
+ JuanMa
--- js/script.js
+++ js/script.js
@@ -1,22 +1,35 @@
-var db, input, ul;
-
 const API_KEY = "2620bb10";
 const URL = "https://www.omdbapi.com/";
 
 window.addEventListener('offline', event =>{
-    document.getElementById('cnx-status').classList.remove('online');
-    document.getElementById('cnx-status').classList.add('offline');   
-  })
+    document.getElementById('statuscnx').innerHTML = (` <div class="offline-indicator" id="cnx-status">
+                                                            <i class="blink-offline"></i>
+                                                        </div>                                                        
+                                                        <p class="nav-item nav-link offline-text"> Estás Offline </p>
+                                                        `);
+});
 
 window.addEventListener('online', event =>{
-    document.getElementById('cnx-status').classList.remove('offline');
-    document.getElementById('cnx-status').classList.add('online');    
-  })
+    document.getElementById('statuscnx').innerHTML = (`<div class="online-indicator" id="cnx-status">
+                                                            <i class="blink-online"></i>
+                                                       </div>
+                                                       <p class="nav-item nav-link online-text"> Estás Online </p>
+                                                       `);
+  });
   
   if(!navigator.onLine){
-    document.getElementById('cnx-status').classList.remove('online');
-    document.getElementById('cnx-status').classList.add('offline');
-  }
+    document.getElementById('statuscnx').innerHTML = (` <div class="offline-indicator" id="cnx-status">
+                                                           <i class="blink-offline"></i>
+                                                       </div>
+                                                       <p class="nav-item nav-link offline-text"> Estás Offline </p>
+                                                       `);
+  } else {
+    document.getElementById('statuscnx').innerHTML = (`<div class="online-indicator" id="cnx-status">
+                                                            <i class="blink-online"></i>
+                                                       </div>                                                       
+                                                       <p class="nav-item nav-link online-text"> Estás Online </p>
+                                                       `);
+  };
 
 $(document).ready(() => {
     $('#searchForm').on('submit', (e) => {
@@ -79,7 +92,7 @@ function getMovie(){
 }
 
 function mostrarDetallesPeli(detalladoJson){
-    console.log(detalladoJson);
+    console.log(detalladoJson);    
     let movie = detalladoJson;    
     movie.Poster === "N/A" ? movie.Poster = 'images/pna.jpg' : movie.Poster;
     movie.Year.includes('–') ? movie.Year += '> Now' : movie.Year;
@@ -96,7 +109,7 @@ function mostrarDetallesPeli(detalladoJson){
           <h3 class="card-header">Información y descripción:</h3>
         </div>
         <ul class="list-group list-group-flush">
-            <li class="list-group-item infoitems"><p><strong>Genre:</strong> ${movie.Genre}</p></li>
+            <li class="list-group-item infoitems"><p><strong>Genre:</strong> ${movie.Genre}</p></li>            
             <li class="list-group-item infoitems"><p><strong>Released:</strong> ${movie.Released}</p></li>
             <li class="list-group-item infoitems"><p><strong>Rated:</strong> ${movie.Rated}</p></li>
             <li class="list-group-item infoitems"><p><strong>IMDB Rating:</strong> ${movie.imdbRating}</p></li>
@@ -106,60 +119,111 @@ function mostrarDetallesPeli(detalladoJson){
         </ul>
         <div class="card-body">
             <a href="index.html" class="btn btn-dark"><i class="fas fa-arrow-left"></i> Volver</a>                        
-            <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-info">Ver ficha IMDB <i class="fab fa-imdb"></i></a>                        
-            <button id="agregarPendientes" type="submit" class="btn btn-success" value="${movie.imdbID}">Sumar a Pendientes <i class="fas fa-bookmark"></i></button>
+            <a id="llaveid" href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-info">Ver ficha IMDB <i class="fab fa-imdb"></i></a>                        
+            <a hre="#" id="addpdn" class="btn btn-success" onclick="lsadd('my_storage','${movie.imdbID}');">Sumar a Pendientes <i class="fas fa-bookmark"></i></a>
         </div>
     </div>
   `;
-  $('#movie').html(exitdescp);
+  $('#movie').html(exitdescp);    
 }
 
-function init(){
-    db = new Dexie("HeroFun-MisPendientes");
-    input = document.getElementById("agregarPendientes").value;    
-    document.body.addEventListener('submit', onSubmit);
-    document.body.addEventListener('click', onClick);
-    
-    db.version(1).stores({ todo: '_id'});
-    db.open()
-      .then(refreshView);
-}
+/* Localstorage para guardar, ver y borrar Mis Pendientes :) - Costó MUCHO pero salió con ayuda de Google.
+Lástima que no pude aplicar indexedDB :( */
 
-function onSubmit(event){
-    event.preventDefault();
+function lsadd(storage_name, id, limit){	
+    var storage_list = [];
+		storage_list = JSON.parse(localStorage.getItem(storage_name)) || [];
+	var storaged 	 = JSON.parse(localStorage.getItem(storage_name));
 
-    db.todo.put({ text: input.value, _id: String(Date.now()) })
-    .then(function(){
-      input.value = '';
-    })
-    .then(refreshView);
-
-  if (flag == 0){
-    flag++;    
-  } else {
-    flag = 0;    
-  }
-}        
-
-
-/* 
-function agregarMisPendientes(){
-
-}
-
-function quitarMisPendientes(){
-
-}
-
-function verMisPendientes(){
-
+	if (localStorage.getItem(storage_name) === null) { 
+			storage_list.push(id); 
+			console.log(storage_list); 
+			localStorage.setItem(storage_name, JSON.stringify(storage_list));
+			process("my_storage");
+			 
+	} else {
+		if(storaged.indexOf(id)==-1){				
+				var count =  storaged.length;
+				if(count >= limit){
+					// Esto es por si alcanza el límite. Me pareció una buena validación 
+					storaged.splice(0, 1); // Remueve el primero, para poder sobre-escribir.
+					localStorage.setItem(storage_name, JSON.stringify(storaged)); // Hace update sin la primera posición ocupada
+					storage_list.push(id); // y agrega el nuevo Pendiente que el usuario clickeó...
+				} else {
+					storage_list.push(id); 
+					console.log(storage_list);
+					localStorage.setItem(storage_name, JSON.stringify(storage_list));	
+				}				
+				process("my_storage");
+				 
+		} else {
+			console.log("Ya se ha agregado!");
+		}
+	}
 }
 
 
+function lsdel(storage_name, id){
+	if (localStorage.getItem(storage_name) === null) { 
+		 console.log("Tu localstorage no está guardado aun");
+	} else {				
+		var ls_data = JSON.parse(localStorage.getItem(storage_name));
+		var index   = ls_data.indexOf(id);		
+		if(index == -1){
+		// si no coincide, sigue	
+		} else {
+			// si coincide el ID, lo saca.
+			ls_data.splice(index, 1);
+			localStorage.setItem(storage_name, JSON.stringify(ls_data));			
+			process("my_storage");
+		}
+	}
+}
 
+function process(storage_name) {
+	let pelipendi = '';
+	if (localStorage.getItem(storage_name) === null) { 
+		console.log("Tu LocalStorage no se generó");	
+	} else { 
+		var storageList = JSON.parse(window.localStorage.getItem(storage_name));
+		var count =  storageList.length;
 
-*/
+		if(count==0){
+			console.log("Tu LocalStorage está vacío");
+            pelipendi = '';
+            $('#pendmovies').html(pelipendi); 
+		}
+		for (var i = 0, len = storageList.length; i < len; i++) {
+		// Mostramos la lista y generamos las vistas de Mis Pendientes
+		
+            let imdbkey = storageList[i];
 
-window.onload = function(){
-    init();
-  }
+            fetch(`${URL}?apikey=${API_KEY}&i=${imdbkey}`).then(function(pendientes)
+            {
+            return pendientes.json();
+           
+            }).then(function(pendientesJson){                
+                         
+               pending = pendientesJson;            
+               pending.Poster === "N/A" ? pending.Poster = 'images/pna.jpg' : pending.Poster;
+               pending.Year.includes('–') ? pending.Year += '> Now' : pending.Year;
+               pelipendi += `<div class="card border-primary mb-3 tarjetas" style="max-width: 20rem;">
+               <div class="card-header">${pending.Type} | ${pending.Year}</div>
+               <div class="card-body">                            
+                   <img class="img-fluid" src="${pending.Poster}" alt="Poster de ${pending.Title}">
+                   <h4 class="card-title">${pending.Title}</h4>                                               
+               </div>                                                
+               <div class="d-grid gap-2 button-effect">
+                   <a href="#" class="btn btn-lg btn-primary botonQuitar" onclick="lsdel('my_storage','${pending.imdbID}');">Quitar de Mis Pendientes</a>
+               </div>
+           </div>`;
+           $('#pendmovies').html(pelipendi);     
+           })       
+           .catch(function(error){
+           console.log('No existe la serie/película o la chingaste --> ', error);
+            })
+           }}
+	}
+
+// meto update de todo
+process("my_storage");
